093cdefd9c42dad16e64a2920b3708dc763cb837
bearboy80 <2373319771@qq.com>
2024-03-25 16:55:50 +0800

    add eureka meta data upload

---
+++ biz-web/src/main/java/com/github/bearboy/biz/meta/EurekaMetaDataUpload.java
@@ -0,0 +1,37 @@
+package com.github.bearboy.biz.meta;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.InstanceInfo;
+import com.sun.management.OperatingSystemMXBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.util.ClassUtils;
+
+import java.lang.management.ManagementFactory;
+
+@Configuration(proxyBeanMethods = false)
+public class EurekaMetaDataUpload {
+    private final static boolean HOTSPOT_JVM = ClassUtils.isPresent(String.valueOf(OperatingSystemMXBean.class), null);
+    @Autowired
+    private ApplicationInfoManager applicationInfoManager;
+
+    @Scheduled(fixedRate = 1000 * 40)
+    public void uploadMetaData() {
+        InstanceInfo info = applicationInfoManager.getInfo();
+        info.getMetadata().put("cpu-usage", getCpuUsage());
+        info.setIsDirty();
+        System.out.println("开始上报meta 数据到 eureka server");
+    }
+
+    String getCpuUsage() {
+        if (HOTSPOT_JVM) {
+            OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean)
+                    ManagementFactory.getOperatingSystemMXBean();
+            double usage = operatingSystemMXBean.getCpuLoad() * 100;
+            return String.valueOf(usage);
+        } else {
+            return "0";
+        }
+    }
+}
